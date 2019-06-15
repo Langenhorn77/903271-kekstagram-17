c52d440c4c36c85eea5a3413282231195efde2ba
Langenhorn77 <ch_ur@mail.ru>
2019-06-15 12:28:01 +0300

    5. Личный проект: пока все дома

--- js/main.js
+++ js/main.js
@@ -1,32 +1,12 @@
 'use strict';
 
-var pictureTemplate = document.getElementById('picture')
-  .content
-  .querySelector('.picture');
-
-var picturesListElement = document.querySelector('.pictures');
-
-var fragment = document.createDocumentFragment();
-
-var PHOTO_NUMBER = 25;
-var AVATAR_NUMBER = 6;
-var MIN_LIKES = 15;
-var MAX_LIKES = 200;
-var MIN_COMMENTS = 1;
-var MAX_COMMENTS = 20;
-
-var PICTURES = [];
-var PHOTOS = [];
-var AVATARS = [];
-var COMMENTS = [];
-
 var MESSAGE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
 var NAMES = [
@@ -42,70 +22,79 @@ var NAMES = [
   'Сергей',
   'Владимир',
   'Елена',
-  'Юрий',
+  'Юрий'
 ];
+
+var PHOTO_NUMBER = 25;
+var AVATAR_NUMBER = 6;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
+var MIN_COMMENTS = 1;
+var MAX_COMMENTS = 20;
+
+var pictures = [];
+
+var pictureTemplate = document.getElementById('picture')
+  .content
+  .querySelector('.picture');
+
+var picturesListElement = document.querySelector('.pictures');
+
+var fragment = document.createDocumentFragment();
 
 var getRandomIndex = function (min, max) {
   return min + Math.floor(Math.random() * (max + 1 - min));
 };
 
-var fillPhotoArray = function (number, array, path, type) {
-  for (var j = 1; j <= number; j++) {
-    var url = {
-      url: path + j + '.' + type
-    };
-    array.push(url);
-  }
-};
 
-var fillCommentArray = function (number, array) {
-  for (var i = 0; i < number; i++) {
+var fillCommentArray = function () {
+  var comments = [];
+  for (var i = 1; i < getRandomIndex(1, AVATAR_NUMBER); i++) {
     var comment = {
-      avatar: AVATARS[getRandomIndex(0, AVATARS.length - 1)].url,
+      avatar: 'img/avatar-' + i + '.svg',
       message: MESSAGE[getRandomIndex(0, MESSAGE.length - 1)],
       name: NAMES[getRandomIndex(0, NAMES.length - 1)]
     };
-    array.push(comment);
+    comments.push(comment);
+  }
+  return comments;
+};
+
+var addComments = function (index) {
+  for (var j = 0; j < getRandomIndex(MIN_COMMENTS, MAX_COMMENTS); j++) {
+    pictures[index].comments = fillCommentArray();
   }
 };
 
-var fillArray = function (number, array, jmin, jmax) {
-  for (var i = 0; i < number; i++) {
-    var photo = {
-      url: PHOTOS[getRandomIndex(0, PHOTOS.length - 1)].url,
+var fillArray = function () {
+  for (var i = 0; i < PHOTO_NUMBER; i++) {
+    pictures.push({
+      url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandomIndex(MIN_LIKES, MAX_LIKES),
       comments: []
-    };
-    var count = getRandomIndex(jmin, jmax);
-    for (var j = 0; j < count; j++) {
-      photo.comments[j] = COMMENTS[getRandomIndex(0, COMMENTS.length - 1)];
-    }
-    array.push(photo);
+    });
+    addComments(i);
   }
 };
+
+fillArray();
 
 var renderPicture = function (picture) {
   var pictureElement = pictureTemplate.cloneNode(true);
+
   pictureElement.querySelector('.picture__img').src = picture.url;
   pictureElement.querySelector('.picture__likes').textContent = picture.likes;
   pictureElement.querySelector('.picture__comments').textContent = picture.comments.length.toString();
+
   return pictureElement;
 };
 
 var addPicture = function () {
-  for (var i = 0; i < PICTURES.length; i++) {
-    fragment.appendChild(renderPicture(PICTURES[i]));
+  for (var i = 0; i < pictures.length; i++) {
+    fragment.appendChild(renderPicture(pictures[i]));
   }
-};
 
-
-var newElement = function () {
   picturesListElement.appendChild(fragment);
 };
 
-fillPhotoArray(PHOTO_NUMBER, PHOTOS, 'photos/', 'jpg');
-fillPhotoArray(AVATAR_NUMBER, AVATARS, 'img/avatar-', 'svg');
-fillCommentArray(PHOTO_NUMBER, COMMENTS);
-fillArray(PHOTO_NUMBER, PICTURES, MIN_COMMENTS, MAX_COMMENTS);
 addPicture();
-newElement();
