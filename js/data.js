32842b09faeff8e8dea24d22daeaaa3f0ee529d7
Langenhorn77 <ch_ur@mail.ru>
2019-06-26 19:38:27 +0300

    5. Личный проект: подробности

--- js/data.js
+++ js/data.js
@@ -34,40 +34,38 @@
   var MIN_COMMENTS = 1;
   var MAX_COMMENTS = 20;
 
-
-  var fillCommentArray = function () {
-    var comments = [];
-    for (var i = 1; i < window.utils.getRandomIndex(1, AVATAR_NUMBER); i++) {
-      var comment = {
-        avatar: 'img/avatar-' + i + '.svg',
-        message: MESSAGE[window.utils.getRandomIndex(0, MESSAGE.length - 1)],
-        name: NAMES[window.utils.getRandomIndex(0, NAMES.length - 1)],
-      };
-      comments.push(comment);
-    }
-    return comments;
-  };
-
-  var addComments = function (index) {
-    for (var j = 0; j < window.utils.getRandomIndex(MIN_COMMENTS, MAX_COMMENTS); j++) {
-      window.data.pictures[index].comments = fillCommentArray();
-    }
-  };
-
-  var fillArray = function () {
-    for (var i = 0; i < PHOTO_NUMBER; i++) {
-      window.data.pictures.push({
-        url: 'photos/' + (i + 1) + '.jpg',
-        likes: window.utils.getRandomIndex(MIN_LIKES, MAX_LIKES),
-        comments: [],
-      });
-      addComments(i);
-    }
-  };
-
-  fillArray();
-
   window.data = {
     pictures: [],
+
+    fillCommentArray: function () {
+      var comments = [];
+      for (var i = 1; i < window.utils.getRandomIndex(1, AVATAR_NUMBER); i++) {
+        var comment = {
+          avatar: 'img/avatar-' + i + '.svg',
+          message: MESSAGE[window.utils.getRandomIndex(0, MESSAGE.length - 1)],
+          name: NAMES[window.utils.getRandomIndex(0, NAMES.length - 1)],
+        };
+        comments.push(comment);
+      }
+      return comments;
+    },
+
+    addComments: function (index) {
+      for (var j = 0; j < window.utils.getRandomIndex(MIN_COMMENTS, MAX_COMMENTS); j++) {
+        window.data.pictures[index].comments = window.data.fillCommentArray();
+      }
+    },
+
+    fillArray: function () {
+      for (var i = 0; i < PHOTO_NUMBER; i++) {
+        window.data.pictures.push({
+          url: 'photos/' + (i + 1) + '.jpg',
+          likes: window.utils.getRandomIndex(MIN_LIKES, MAX_LIKES),
+          comments: [],
+        });
+        window.data.addComments(i);
+      }
+    },
   };
+  window.data.fillArray();
 })();
